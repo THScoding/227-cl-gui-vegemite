4d5a4c79f98be974fb0e2a94fddc5d2da48bcc60
Thaddeus Spitzer <thaddeus.spitzer@crpusd.org>
2026-01-12 10:01:45 -0800

    work?

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -5,7 +5,7 @@ from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
 def do_command():
-    command = ["ping", "localhost"]
+    command = ["ping", "www.pltw.org"]
     # Windows version to limit to 4 requests: command = ["ping", "localhost", "-n", "4"]
     # Mac version to limit to 4 requests:     command = ["ping", "localhost", "-n", "4"]
     
@@ -36,8 +36,30 @@ frame = tk.Frame(root, bg = "black") # change frame color
 frame.pack()
 
 # set up button to run the do_command function
-ping_btn = tk.Button(frame, text = "ping", command = do_command)
+# CODE TO ADD
+# Makes the command button pass it's name to a function using lambda
+ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", command=lambda:do_command())
 ping_btn.pack()
+
+# creates the frame with label for the text box
+frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
+frame_URL.pack()
+
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+    compound="center",
+    font=("comic sans", 14),
+    bd=0, 
+    relief=tk.FLAT, 
+    cursor="heart",
+    fg="mediumpurple3",
+    bg="black")
+url_label.pack(side=tk.LEFT)
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry.pack(side=tk.LEFT)
+
+frame = tk.Frame(root,  bg="black") # change frame color
+frame.pack()
 
 # Adds an output box to GUI.
 command_textbox = tksc.ScrolledText(frame, height=10, width=100)

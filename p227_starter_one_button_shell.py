c512e1094ecd196f5f706f94a32af2782695a4f7
Thaddeus Spitzer <thaddeus.spitzer@crpusd.org>
2026-01-12 10:14:16 -0800

    trying to allow url to be entered

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -4,35 +4,20 @@ import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
+def retreive_url():
+    url = url_entry.get()
+    return url
+
 def do_command():
-    command = ["ping", "www.pltw.org"]
+    global url
+    command = ["ping", url]
     # Windows version to limit to 4 requests: command = ["ping", "localhost", "-n", "4"]
     # Mac version to limit to 4 requests:     command = ["ping", "localhost", "-n", "4"]
-    
+    url = retreive_url()
     sp.run(command)
 
 root = tk.Tk()
 frame = tk.Frame(root)
-frame.pack()
-
-# creates the frame with label for the text box
-frame_URL = tk.Frame(root, pady = 10,  bg = "black") # change frame color
-frame_URL.pack()
-
-# decorative label
-url_label = tk.Label(frame_URL, text = "Enter a URL of interest: ", 
-    compound = "center",
-    font = ("comic sans", 14),
-    bd = 0, 
-    relief = tk.FLAT, 
-    cursor = "heart",
-    fg = "mediumpurple3",
-    bg = "black")
-url_label.pack(side = tk.LEFT)
-url_entry = tk.Entry(frame_URL,  font = ("comic sans", 14)) # change font
-url_entry.pack(side = tk.LEFT)
-
-frame = tk.Frame(root, bg = "black") # change frame color
 frame.pack()
 
 # set up button to run the do_command function

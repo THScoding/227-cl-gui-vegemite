876599663b1a619776f8f425fa91d042358e7086
Thaddeus Spitzer <thaddeus.spitzer@crpusd.org>
2026-01-14 09:16:50 -0800

    Visual Changes

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -56,27 +56,27 @@ ping_btn = tk.Button(frame, text="Check to see if a URL is up and active",
     command=lambda:Submit(),
     compound="center",
     font=("comic sans", 12),
-    bd=0, 
+    bd=5, 
     relief="flat",
     cursor="heart",
     bg="white", activebackground="gray")
 ping_btn.pack() 
 
 # creates the frame with label for the text box
-frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
+frame_URL = tk.Frame(root, pady=10,  bg="white") # change frame color
 frame_URL.pack()
 
 # decorative label
 url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
     compound="center",
     font=("comic sans", 14),
-    bd=0, 
+    bd=5, 
     relief=tk.FLAT, 
     cursor="heart",
-    fg="mediumpurple3",
-    bg="black")
+    fg="black",
+    bg="white")
 url_label.pack(side=tk.LEFT)
-url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14), bd=5) # change font
 url_entry.pack(side=tk.LEFT)
 
 frame = tk.Frame(root,  bg="black") # change frame color

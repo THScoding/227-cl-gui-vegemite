58312e60d29e7d297089162f59893e5894c23fbf
Noah Wisner <noah.wisner@crpusd.org>
2026-01-15 09:36:12 -0800

    Better readability

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -21,7 +21,7 @@ def do_command(command):
     command_textbox.insert(tk.END, command + " working....\n")
     command_textbox.update()
 
-    with sp.Popen(command + ' ' + url_val, stdout=sp.PIPE, bufsize=1, universal_newlines=True) as p:
+    with sp.Popen(command + ' ' + url_val, stdout = sp.PIPE, bufsize = 1, universal_newlines = True) as p:
         for line in p.stdout:
             command_textbox.insert(tk.END,line)
             command_textbox.update()
@@ -42,7 +42,7 @@ frame.pack()
 
 def Submit(): 
     global url_val
-    res=messagebox.askquestion("POP UP BOX",
+    res = messagebox.askquestion("POP UP BOX",
         "Are you sure that's the right url?")
     if res == "yes":
         url_val = url_entry.get()
@@ -57,14 +57,14 @@ def make_new_frame():
     frame1.pack()
     command_textbox = tksc.ScrolledText(frame1, height=10, width=100)
     command_textbox.pack()
-    ping_btn = tk.Button(frame1, text="Ping", 
-    command=lambda:do_command("ping"),
-    compound="center",
-    font=("comic sans", 12),
-    bd=5, 
-    relief="flat",
-    cursor="gumby",
-    bg="white", activebackground="gray")
+    ping_btn = tk.Button(frame1, text = "Ping", 
+    command = lambda:do_command("ping"),
+    compound = "center",
+    font = ("comic sans", 12),
+    bd = 5, 
+    relief = "flat",
+    cursor = "gumby",
+    bg = "white", activebackground = "gray")
     ping_btn.pack()
 # set up button to run the do_command function
 # CODE TO ADD
@@ -72,33 +72,33 @@ def make_new_frame():
 # CODE TO ADD
 # Modifies the ping button parameters.
 submit_btn = tk.Button(frame, text="Check to see if a URL is up and active", 
-    command=lambda:Submit(),
-    compound="center",
-    font=("comic sans", 12),
-    bd=5, 
-    relief="flat",
-    cursor="gumby",
-    bg="white", activebackground="gray")
+    command = lambda:Submit(),
+    compound = "center",
+    font = ("comic sans", 12),
+    bd = 5, 
+    relief = "flat",
+    cursor = "gumby",
+    bg = "white", activebackground = "gray")
 submit_btn.pack() 
 
 # creates the frame with label for the text box
-frame_URL = tk.Frame(root, pady=10,  bg="white") # change frame color
+frame_URL = tk.Frame(root, pady = 10,  bg = "white") # change frame color
 frame_URL.pack()
 
 # decorative label
 url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
-    compound="center",
-    font=("comic sans", 14),
-    bd=5, 
-    relief=tk.FLAT, 
-    cursor="gumby",
-    fg="black",
-    bg="white")
-url_label.pack(side=tk.LEFT)
-url_entry= tk.Entry(frame_URL,  font=("comic sans", 14), bd=5) # change font
-url_entry.pack(side=tk.LEFT)
+    compound = "center",
+    font = ("comic sans", 14),
+    bd = 5, 
+    relief = tk.FLAT, 
+    cursor = "gumby",
+    fg = "black",
+    bg = "white")
+url_label.pack(side = tk.LEFT)
+url_entry = tk.Entry(frame_URL,  font = ("comic sans", 14), bd = 5) # change font
+url_entry.pack(side = tk.LEFT)
 
-frame = tk.Frame(root,  bg="black") # change frame color
+frame = tk.Frame(root,  bg= " black") # change frame color
 frame.pack()
 
 # Adds an output box to GUI.
